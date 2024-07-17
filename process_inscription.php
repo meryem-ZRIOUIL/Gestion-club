29967a9065ef81242db1a1065786f945c4c08f7e
meryem zriouil <meryem.zriouil@uit.ac.ma>
2024-07-17 21:10:50 +0100

    Update process_inscription.php

--- process_inscription.php
+++ process_inscription.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nom = $_POST['nom'];
     $prenom = $_POST['prenom'];
     $email = $_POST['email'];
-    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); // Hachage du mot de passe
+    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); 
 
     $sql = "INSERT INTO Utilisateur (nom, prenom, email, mot_de_passe) VALUES ('$nom', '$prenom', '$email', '$mot_de_passe')";
 
