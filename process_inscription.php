7fe435adc22935cbe48b1cb540a8006e7c92675b
meryem zriouil <meryem.zriouil@uit.ac.ma>
2024-07-17 21:31:19 +0100

    Update process_inscription.php

--- process_inscription.php
+++ process_inscription.php
@@ -1,20 +1,29 @@
 <?php
-include 'connexion.php';
+include 'connexion.php'; 
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); 
+    // Sanitize and retrieve form data
+    $nom = htmlspecialchars($_POST['nom']);
+    $prenom = htmlspecialchars($_POST['prenom']);
+    $email = htmlspecialchars($_POST['email']);
+    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT);
+    $sql = $conn->prepare("INSERT INTO Utilisateur (nom, prenom, email, mot_de_passe) VALUES (?, ?, ?, ?)");
+    
+    $sql->bind_param("ssss", $nom, $prenom, $email, $mot_de_passe);
 
-    $sql = "INSERT INTO Utilisateur (nom, prenom, email, mot_de_passe) VALUES ('$nom', '$prenom', '$email', '$mot_de_passe')";
-
-    if ($conn->query($sql) === TRUE) {
+   
+    if ($sql->execute()) {
         echo "Nouvel enregistrement créé avec succès";
     } else {
-        echo "Erreur : " . $sql . "<br>" . $conn->error;
+        echo "Erreur : " . $sql->error;
     }
 
-    $conn->close();
+    
+    $sql->close();
+} else {
+    echo "Méthode non autorisée."; 
 }
+
+$conn->close();
 ?>
