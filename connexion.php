8547659ae6011d3cc38b738618390c587f34f1ba
meryem zriouil <meryem.zriouil@uit.ac.ma>
2024-07-17 20:38:20 +0100

    Add files via upload

---
+++ connexion.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "gestion_club";
+
+// Créer une connexion
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Vérifier la connexion
+if ($conn->connect_error) {
+    die("Connexion échouée : " . $conn->connect_error);
+} else {
+    echo "Connexion réussie";
+}
+?>
