40ed36d5e699f4dfadbc04416bef58d58105e5ff
meryem zriouil <meryem.zriouil@uit.ac.ma>
2024-07-17 20:39:08 +0100

    Add files via upload

---
+++ process_inscription.php
@@ -0,0 +1,20 @@
+<?php
+include 'connexion.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $email = $_POST['email'];
+    $mot_de_passe = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT); // Hachage du mot de passe
+
+    $sql = "INSERT INTO Utilisateur (nom, prenom, email, mot_de_passe) VALUES ('$nom', '$prenom', '$email', '$mot_de_passe')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Nouvel enregistrement créé avec succès";
+    } else {
+        echo "Erreur : " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
